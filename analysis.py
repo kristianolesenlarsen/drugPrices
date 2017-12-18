fc66f83a65335b4dbc17f302f4dd741393b633f8
Kristian <kristianuruplarsen@gmail.com>
2017-12-18 22:22:37 +0100

    new averages

--- analysis.py
+++ analysis.py
@@ -2,6 +2,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import dates
+import datetime
+import math
+import numpy as np
 
 plt.style.use('bmh')
 df = pd.read_csv('raw_data.csv')
@@ -22,6 +25,55 @@ def spread(df, key, value):
     return df.pivot_table(index = keep_cols, columns = key, values = value).reset_index().rename_axis(None, axis=1)
 
 
+# rework this to handle panel data!
+def diff(col, index, D):
+    """ return a dataframe with nth differenced vectors
+    args:
+        col: a column to difference
+        L: list or int indicating which differences to return
+    """
+    out = {'index': index,
+           'value': col}
+
+    # this just ensures L is a list
+    if type(D) == int:
+        D = [D]
+
+    for diff in D:
+        diffed = [i - di for i, di in zip(col[diff:], col[:-diff])]
+        # insert NaN's in the beginning of the column
+        for i in range(len(diffed),len(col)):
+            diffed.insert(0, np.nan)
+        # add the lag to out dict
+        out['L' + str(diff)] = diffed
+
+    return pd.DataFrame(out)
+
+
+def lag(col, index, L):
+    """ return a dataframe with nth lagged vectors
+    args:
+        col: a column to lag
+        L: list or int indicating which lags to return
+    """
+    out = {'index': index,
+           'value': col}
+
+    # this just ensures L is a list
+    if type(L) == int:
+        L = [L]
+
+    for lag in L:
+        lagged = col.tolist()[:-lag]
+
+        for i in range(len(lagged), len(col)):
+            lagged.insert(0, np.nan)
+
+        out['L' + str(lag)] = lagged
+
+    return pd.DataFrame(out)
+
+
 # gather all the dates
 df2 = gather(df, 'time', 'price', [c for c in df.columns if c.isdigit()])
 
@@ -32,17 +84,77 @@ df3 = spread(df2, 'Variabel', 'price')
 df3['time'] = pd.to_datetime(df3['time'])
 df3['date'] = df3['time'].apply(lambda x: datetime.date(x.year, x.month, x.day))
 
+
+
+from sklearn import linear_model as lm
+
+
+# these operations create a dataset of first differenced AIP prices
+df41 = df3.groupby('date')['aip'].mean().reset_index()
+
+dfdiff = diff(df41['aip'], df41['date'], range(1,10))
+dflag = lag(dfdiff['L1'], dfdiff['index'], range(1,10))[10:]
+
+
+#fig, (ax1, ax2) = plt.subplots(nrows = 2, gridspec_kw = {'height_ratios': [3:1]})
+fig = plt.figure(1)
+plt.rc('text', usetex = True)
+
+ax1 = plt.subplot2grid((3,1), (0,0), rowspan = 2)
+ax2 = plt.subplot2grid((3,1), (2,0), rowspan = 1, sharex = ax1)
+ax1.set_title(r'Medicine prices over time')
+#plt.title('Medicine prices over time\n')
+ax2.plot(
+    dflag['index'],
+    dflag['L1'],
+    label = r'$\Delta$AUP'
+)
+
+ax1.set_ylabel(r'DKK')
+
+ax2.plot(dflag['index'],
+        [np.mean(dflag['L1']) for i in dflag['index']],
+        color = 'r',
+        linestyle = ':')
+
 # PLOT SIMPLE AVERAGES
 # this simply replicates the work also done in descriptive.R
-plt.plot(df3.groupby('date')['aip'].mean().reset_index()['date'],
+ax1.plot(df3.groupby('date')['aip'].mean().reset_index()['date'],
          df3.groupby('date')['aip'].mean().reset_index()['aip'],
-         label = 'AIP')
-plt.plot(df3.groupby('date')['aup'].mean().reset_index()['date'],
+         label = r'AIP')
+ax1.plot(df3.groupby('date')['aup'].mean().reset_index()['date'],
          df3.groupby('date')['aup'].mean().reset_index()['aup'],
-         label = "AUP")
-plt.title('Medicine prices over time\n')
-plt.legend(loc = 3)
-plt.ylabel('Price (DKK)')
+         label = r'AUP')
+
+# remove x axis marks for large figure
+plt.setp(ax1.get_xticklabels(), visible=False)
+ax1.legend(loc = 4)
+ax2.legend(loc = 2)
+
+fig.tight_layout()
+plt.savefig('averages.png')
+plt.show()
+
+# Plot profit margin of the pharmacies
+df3['relPrice'] = df3['aup']/df3['aip']
+
+plt.plot(df3.groupby('date')['relPrice'].mean().reset_index()['date'],
+         df3.groupby('date')['relPrice'].mean().reset_index()['relPrice'],
+         label = "AUP/AIP")
+plt.title("Profit margin")
 plt.xlabel('Year')
-plt.savefig('img/averages.png')
+plt.ylabel('margin [100ths]')
+plt.legend(loc = 3)
+plt.show()
+
+
+# plot prices against each other
+df3['log_aip'] = df3['aip'].apply(lambda x: math.log(x))
+df3['log_aup'] = df3['aup'].apply(lambda x: math.log(x))
+
+plt.scatter(df3.groupby('date')['log_aup'].mean().reset_index()['log_aup'],
+         df3.groupby('date')['log_aip'].mean().reset_index()['log_aip'],
+         edgecolors = 'b',
+         facecolors = "None")
+
 plt.show()
